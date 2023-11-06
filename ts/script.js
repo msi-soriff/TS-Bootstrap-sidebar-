150d192542f8a417a26732aa8c09925ee001b3fa
msi-soriff <m.sorif.islam00@gmail.com>
2023-11-06 20:21:15 +0600

    creating a simple TS-Bootstrap sidebar

---
+++ ts/script.js
@@ -0,0 +1,12 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector("#sidebarToggle");
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener("click", (event) => {
+            event.preventDefault();
+            document.body.classList.toggle("sb-sidenav-toggled");
+            localStorage.setItem("sb|sidebar-toggle", document.body.classList.contains("sb-sidenav-toggled").toString());
+        });
+    }
+});
---
+++ ts/script.ts
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Toggle the side navigation
+  const sidebarToggle =
+    document.body.querySelector<HTMLButtonElement>("#sidebarToggle");
+  if (sidebarToggle) {
+    sidebarToggle.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.toggle("sb-sidenav-toggled");
+      localStorage.setItem(
+        "sb|sidebar-toggle",
+        document.body.classList.contains("sb-sidenav-toggled").toString()
+      );
+    });
+  }
+});
